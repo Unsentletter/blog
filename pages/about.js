47db81123f2ccceb7c9fb97b521f9561e1b867fb
Tristan Southwell <tristan.southwell@hotmail.com>
2019-07-20 22:45:23 +1000

    Added photo of my sexy self to about me page

--- pages/about.js
+++ pages/about.js
@@ -7,16 +7,42 @@ class About extends Component {
 			<div>
 				<style jsx>
 					{`
-						.header {
+						.body {
+							height: 100vh;
+							background-color: #f5f5f5;
+							display: flex;
+							flex-direction: row;
+							margin-top: -80px;
+							padding-top: 80px;
 						}
-						.test {
+						.left {
+							width: 50%;
+							padding: 50px;
+							padding-top: 0;
+							text-align: center;
+						}
+						img {
+							border-radius: 50%;
+							max-width: 100%;
+							max-height: 100%;
+							margin-top: -20px;
+						}
+						.right {
+							width: 50%;
 						}
 					`}
 				</style>
 				<Layout>
-					<h1>
-						This will be an about me page where I talk about how good I am
-					</h1>
+					<div className="body">
+						<div className="left">
+							<img src="static/me.png" alt="This is me" />
+						</div>
+						<div className="right">
+							<h1>
+								This will be an about me page where I talk about how good I am
+							</h1>
+						</div>
+					</div>
 				</Layout>
 			</div>
 		);
